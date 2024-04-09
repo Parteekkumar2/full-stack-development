1320dc90d305dbed5a6dea7d3389552787200fd8
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-09 15:30:11 +0530

    Add new Template literals concept

--- JAVASCRIPT Part-2/app.js
+++ JAVASCRIPT Part-2/app.js
@@ -2,3 +2,12 @@ console.log("Hello World !");
 let y=4;
 let b=3;
 console.log("sum is :", y+b);
+
+//Template literals
+let pencilprice=10;
+let eraserprice=5;
+//let output="The total price:" +(pencilprice+eraserprice)+"Rupees.";
+//let output=`The total price is :${pencilprice+eraserprice} Rupees.`;
+//console.log(output);
+
+console.log(`The total price is :${pencilprice+eraserprice} Rupees.`);
