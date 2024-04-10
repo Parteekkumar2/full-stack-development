a70967fbc54b33200080a1dda6d02e1977598411
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-10 15:12:04 +0530

    Add Practice question

--- JAVASCRIPT Part-2/app.js
+++ JAVASCRIPT Part-2/app.js
@@ -83,4 +83,14 @@ if (colors==="red"){
 }
 
 
-
+//practice question if elseif else 
+let size="xl";
+if(size==="xl"){
+    console.log("Price is Rs.250");
+}else if(size==="l"){
+    console.log("Price is Rs.200");
+}else if(size==="m"){
+    console.log("Price is Rs.100");
+}else{
+    console.log("Price is Rs.50");
+}
