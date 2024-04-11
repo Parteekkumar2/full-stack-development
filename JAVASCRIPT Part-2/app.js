d6c069f895e199aa7040ac4f789d2c9aaa71749d
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-11 07:32:42 +0530

    Add pracice qs

--- JAVASCRIPT Part-2/app.js
+++ JAVASCRIPT Part-2/app.js
@@ -107,3 +107,11 @@ if(mark>=33){
 }else{
     console.log("Better luck next time");
 }
+
+//Practice question
+let str="apple";
+if((str[0]==='a') && (str.length>3)){
+    console.log("good")
+}else{
+    console.log("not a good string");
+}
