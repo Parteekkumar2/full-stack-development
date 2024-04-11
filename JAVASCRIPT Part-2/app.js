f03449e5649cb2a6cf0d90dcd6c3bd67936f19b3
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-11 07:36:16 +0530

    Add new practice qs

--- JAVASCRIPT Part-2/app.js
+++ JAVASCRIPT Part-2/app.js
@@ -115,3 +115,12 @@ if((str[0]==='a') && (str.length>3)){
 }else{
     console.log("not a good string");
 }
+
+//practice qs
+
+let num=12;
+if((num%3===0) && ((num+1==15)||(num-1==11))){
+    console.log("safe");
+}else{
+    console.log("unsafe");
+}
