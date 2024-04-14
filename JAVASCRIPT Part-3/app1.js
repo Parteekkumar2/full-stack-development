f024f70d67ff7d67e0fb262e76be1b7043d34775
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-14 21:18:46 +0530

    Add new Method chaining

--- JAVASCRIPT Part-3/app1.js
+++ JAVASCRIPT Part-3/app1.js
@@ -12,3 +12,14 @@ console.log(name.toUpperCase());
 //indexOf method
 let x="ILoveYou";
 //x.indexOf("Love")  
+
+//Method chaining
+
+let y="   hello   ";
+// let newy=y.trim();
+// console.log("after trim:",newy);
+// newy=newy.toUpperCase();
+// console.log("after upper case:",newy);
+//         or
+let newy=y.trim().toUpperCase();
+console.log(newy);
