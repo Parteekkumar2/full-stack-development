b6d6100d1068b235fb3da49fa3c49a313e1f06a5
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-21 08:25:38 +0530

    Add Print all odd number (1 to 15)

--- JAVASCRIPT Part-4/index.js
+++ JAVASCRIPT Part-4/index.js
@@ -5,3 +5,13 @@ for(let i=1;i<=5;i++){
 for(let i=10;i>=1;i--){
     console.log(i);
 }
+//print all odd number(1to 15)
+for(let i=1;i<=15;i=i+2){
+    console.log(i);
+}
+
+console.log("backwards");
+for(let i=15;i>=1;i=i-2){
+    console.log(i);
+}
+
