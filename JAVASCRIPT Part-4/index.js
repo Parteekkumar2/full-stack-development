b691e7c01dd51e44869f7432aa52360dc3435153
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-23 15:42:25 +0530

    Add new nested loop concept

--- JAVASCRIPT Part-4/index.js
+++ JAVASCRIPT Part-4/index.js
@@ -37,3 +37,10 @@ n=parseInt(n);
 for(let i=n;i<=n*10;i=i+n){
     console.log(i);
 }
+//nested loop
+for(let i=1;i<=3;i++){
+    console.log(`outer loop ${i}`);
+    for(let j=1;j<=3;j++){
+        console.log(j);
+    }
+}
