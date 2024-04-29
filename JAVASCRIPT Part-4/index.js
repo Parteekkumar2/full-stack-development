7e76b2f51de413cd84e509c79f63f75328685bc3
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-29 07:54:35 +0530

    Add new reverse concept

--- JAVASCRIPT Part-4/index.js
+++ JAVASCRIPT Part-4/index.js
@@ -92,3 +92,8 @@ let fruits=["mango","apple","banana","litchi","orange"];
 for(let i=0;i<fruits.length;i++){
     console.log(i,fruits[i]);
 }
+
+//reverse loop with array
+for(let i=fruits.length-1;i>0;i--){
+    console.log(i,fruits[i]);
+}
