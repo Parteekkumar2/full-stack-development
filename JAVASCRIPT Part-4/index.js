8e2af872849f7ae64459553cc2b16a960002a4d7
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-29 07:51:59 +0530

    Add loop with arrays concept

--- JAVASCRIPT Part-4/index.js
+++ JAVASCRIPT Part-4/index.js
@@ -86,3 +86,9 @@ while(l<=5){
     console.log(l);
     l++;
 }
+
+//loops with arrays
+let fruits=["mango","apple","banana","litchi","orange"];
+for(let i=0;i<fruits.length;i++){
+    console.log(i,fruits[i]);
+}
