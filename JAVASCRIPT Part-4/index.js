d6d9f8de5f1ce1601051875126ead71556aee897
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-04-30 17:40:29 +0530

    Add Eg number 2

--- JAVASCRIPT Part-4/index.js
+++ JAVASCRIPT Part-4/index.js
@@ -106,3 +106,12 @@ for(let i=0;i<heroes.length;i++){
         console.log(`j=${j},${heroes[i][j]}`);
     }
 }
+
+//eg-2
+let student=[["aman",95],["parteek",94.4],["karan",100]];
+for(let i=0;i<student.length;i++){
+    console.log(`info of student #${i}`);
+    for(let j=0;j<student[i].length;j++){
+        console.log(student[i][j]);
+    }
+}
