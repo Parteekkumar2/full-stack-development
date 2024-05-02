03676105e234854b0a5b36544c04b122e5ff9dd7
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-02 17:03:18 +0530

    Add soln 2

--- JAVASCRIPT Part-4/assignment.js
+++ JAVASCRIPT Part-4/assignment.js
@@ -1,3 +1,4 @@
+//soln -1
 let arr=[1,2,3,4,5,6,2,3];
 let num=2;
 for(let i=0;i<arr.length;i++){
@@ -6,3 +7,13 @@ if(arr[i]==num){
 }
 }
 console.log(arr);
+
+//soln-2
+let number=287152;
+let count=0;
+let copy=number;
+while(copy>0){
+    count++;
+    copy=Math.floor(copy/10);
+}
+console.log(count);
