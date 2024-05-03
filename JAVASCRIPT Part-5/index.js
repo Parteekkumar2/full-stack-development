a1798fcacdc79489e7f17940a595b591768694d1
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-03 07:23:45 +0530

    Add Creating Object Literals

---
+++ JAVASCRIPT Part-5/index.js
@@ -0,0 +1,15 @@
+//Creating Object literals
+let student={
+    name: "Parteek",
+    age:18,
+    marks:80,
+    city:"Haryana"
+};
+console.log(student);
+
+//example -2
+let delhi={
+    latitude: "28.7041",
+    longitude:[256.98,7455.87,54665.785]
+};
+console.log(delhi);
