cf29dd3b2b53cfe650d13f11a6704231f8704b1e
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-05 07:04:24 +0530

    ADD soln - 1,2

---
+++ JAVASCRIPT Part-5/Assignment.js
@@ -0,0 +1,12 @@
+//soln -1
+let dice =Math.floor(Math.random()*6)+1;
+console.log(dice);
+
+//soln -2
+
+const car={
+    name:"Maruti suzuki",
+    model:"Maruti suzuki dzire",
+    color:"white"
+};
+console.log(car.name);
