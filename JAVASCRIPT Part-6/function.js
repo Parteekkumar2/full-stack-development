598001e8cc6d32adafa3fd2efd98db33a890accb
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-05 16:58:59 +0530

    Add practice question -6

--- JAVASCRIPT Part-6/function.js
+++ JAVASCRIPT Part-6/function.js
@@ -106,3 +106,14 @@ function getsum(n){
     }
     return sum;
 }
+
+//Practice question -6
+
+let str=["hi","hello","bye","!"];
+function concat(str){
+    let result="";
+    for(let i=0;i<str.length;i++){
+        result+=str[i];
+    }
+    return result;
+}
