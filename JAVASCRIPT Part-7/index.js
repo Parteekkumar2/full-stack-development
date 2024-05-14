782b7966b14d28ccabfa3a3e4c392fb62d674664
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-14 12:20:55 +0530

    Add this with arrow function concept

--- JAVASCRIPT Part-7/index.js
+++ JAVASCRIPT Part-7/index.js
@@ -58,3 +58,30 @@ console.log("welcome to");
 //       console.log("front end developer");
 //   },2000);
 
+//this with arrow function
+
+const students={
+name:"parteek",
+marks:95,
+prop:this, //global scope
+getname:function(){
+    console.log(this);
+    return this.name;
+},
+getmarks:()=>{
+    console.log(this); //parents scope
+    return this.marks;
+},
+getinfo1:function(){
+    setTimeout(() =>{
+        console.log(this); //student object
+ },2000);
+},
+getinfo2:function(){
+    setTimeout (function(){
+        console.log(this); //window object
+    },2000);
+},
+};
+
+
