ae8961d8056bc4ad6e194406c864c28945f392f9
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-15 15:26:26 +0530

    Array method forEach concept

---
+++ JAVASCRIPT Part-8/index.js
@@ -0,0 +1,34 @@
+//Array Method (foreach,map,filter,some,every,reduce)
+//forEach Method
+let arr=[1,2,3,4,5];
+let print=function(el){
+    console.log(el);
+}
+arr.forEach(print);
+//or
+arr.forEach(function(el){
+    console.log(el);
+});
+//Array function
+arr.forEach((el)=>{
+    console.log(el);
+});
+
+//eg -2 based on object
+let student=[
+    {
+    name:"keshav",
+    marks:95,
+    },
+    {
+        name:"Parteek",
+        marks:94.4,
+    },
+    {
+        name:"Akash",
+        marks:92,
+    },
+];
+student.forEach((student) =>{
+    console.log(student.marks);
+});
