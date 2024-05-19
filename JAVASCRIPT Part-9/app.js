bc3f6549723208888e0f9fd0a2acfe595f21d859
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-19 07:18:30 +0530

    Add selecting Elements By ClassName

---
+++ JAVASCRIPT Part-9/app.js
@@ -0,0 +1,12 @@
+//Selecting ElementByClassName
+
+let smallImages=document.getElementsByClassName("oldImg");
+for(let i=0;i<smallImages.length;i++){
+    console.dir(smallImages[i]);
+}
+
+let smallImage=document.getElementsByClassName("oldImg");
+for(let i=0;i<smallImage.length;i++){
+    smallImage[i].src="assets/spiderman_img.png";
+console.log(`value of image no.  ${i} is changed.`);
+}
