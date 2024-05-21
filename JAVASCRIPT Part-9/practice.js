eda0e7a98f70a3f2b3bed1402cb6fa610735f95b
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-21 22:45:28 +0530

    Add Assignment question

--- JAVASCRIPT Part-9/practice.js
+++ JAVASCRIPT Part-9/practice.js
@@ -23,3 +23,38 @@ div.append(h1);
 div.append(para2);
 div.classList.add("box");
 document.querySelector("body").prepend(div);
+
+//Assignment question
+
+//qs-1
+
+let button=document.createElement("button");
+let input=document.createElement("input");
+
+button.innerText="Click me";
+document.querySelector("body").append(input);
+document.querySelector("body").append(button);
+
+//qs-2
+
+button.setAttribute("id","btn");
+input.setAttribute("placeholder","username");
+
+//qs-3
+let btn=document.querySelector("#btn");
+btn.classList.add("btnstyle");
+
+//qs-4
+
+let h4=document.createElement("h1");
+h4.innerHTML="<u> DOM Practice </u>";
+document.querySelector("body").append(h4);
+
+//qs-5
+
+let para5=document.createElement("p");
+para5.innerHTML="Apna college <b> Delta </b> Practice";
+document.querySelector('body').append(para5);
+
+
+
