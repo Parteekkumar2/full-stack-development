fd23513891aa3e3a79f2738628a4a77034dec4bc
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-21 22:03:54 +0530

    Add part2 practice qs

--- JAVASCRIPT Part-9/practice.js
+++ JAVASCRIPT Part-9/practice.js
@@ -2,3 +2,16 @@ let para1=document.createElement("p");
 para1.innerText="Hey I'am red!";
 document.querySelector('body').prepend(para1);
 para1.classList.add("red");
+
+//2
+let h3=document.createElement("h3");
+h3.innerText="I'am a blue h3!";
+document.querySelector('body').prepend(h3);
+h3.classList.add("blue");
+
+
+//3
+
+let div=document.createElement("div");
+let h1=document.createElement("h1");
+let para2=document.createElement("para2");
