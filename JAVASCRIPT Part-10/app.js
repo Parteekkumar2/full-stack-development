a870d66c4e9dc9f73f92a259db453fa2bbefa2c8
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-23 08:44:58 +0530

    Add onmouseenter concept

--- JAVASCRIPT Part-10/app.js
+++ JAVASCRIPT Part-10/app.js
@@ -1,14 +1,19 @@
 //Mouse / Pointer Events
-let btn=document.querySelector("button");
-console.dir(btn);
-btn.onclick=function(){
-    alert("button was clicked")
-};
+// let btn=document.querySelector("button");
+// console.dir(btn);
+// btn.onclick=function(){
+//     alert("button was clicked")
+// };
 
 //onclick Events
 let btns=document.querySelectorAll("button");
 for(btn of btns){
     btn.onclick=sayHello;
+    //onmouseenter
+    btn.onmouseenter = function() {
+        console.log("you entered a button");
+    };
+    console.dir(btn);
 }
 
 function sayHello(){
