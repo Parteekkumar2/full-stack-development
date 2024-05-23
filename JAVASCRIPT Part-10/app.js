91ea17c433fda91b50823bed99f671ddfa292caf
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-23 08:17:57 +0530

    Add onclick concept

--- JAVASCRIPT Part-10/app.js
+++ JAVASCRIPT Part-10/app.js
@@ -4,3 +4,13 @@ console.dir(btn);
 btn.onclick=function(){
     alert("button was clicked")
 };
+
+//onclick Events
+let btns=document.querySelectorAll("button");
+for(btn of btns){
+    btn.onclick=sayHello;
+}
+
+function sayHello(){
+    alert("Hello");
+}
