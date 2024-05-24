996346e6d2c1b9083805547dbe8a8a9d51ca1785
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-25 00:10:10 +0530

    Keyboard Events concept

--- JAVASCRIPT Part-10/this.js
+++ JAVASCRIPT Part-10/this.js
@@ -12,3 +12,19 @@ btn.addEventListener("click",changeColor);
 p.addEventListener("click",changeColor);
 h1.addEventListener("click",changeColor);
 h3.addEventListener("click",changeColor);
+
+//keyboard event
+
+let inp=document.querySelector("input");
+inp.addEventListener("keydown", function(event){
+    console.log("code=",event.code);
+    if(event.code=="keyU"){
+        console.log("character moves up");
+    }else if(event.code=="keyD"){
+        console.log("character moves down");
+    }else if(event.code=="keyL"){
+        console.log("character moves left");
+    }else if(event.code=="keyR"){
+        console.log("character moves Right");
+    }
+});
