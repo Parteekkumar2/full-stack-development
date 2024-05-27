340248219540a49294af963665828bd48a87063e
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-27 12:49:36 +0530

    Add assignment soln -2

--- JAVASCRIPT Part-10/more.js
+++ JAVASCRIPT Part-10/more.js
@@ -13,3 +13,7 @@ color.addEventListener("mouseout",function(){
 });
 
 
+let btn=document.querySelector("button");
+btn.addEventListener("click",function(){
+    btn.style.backgroundColor="green";
+});
