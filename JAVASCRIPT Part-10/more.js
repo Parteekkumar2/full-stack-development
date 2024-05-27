66b0f3e6ee7a1bc2af153160908ac6966da7825f
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-27 12:43:54 +0530

    Add assignment question

--- JAVASCRIPT Part-10/more.js
+++ JAVASCRIPT Part-10/more.js
@@ -5,3 +5,11 @@ inp.addEventListener("input", function(){
     console.log(inp.value);
     p.innerText=inp.value;
 });
+
+
+let color=document.querySelector("#color");
+color.addEventListener("mouseout",function(){
+    this.style.color="orange";
+});
+
+
