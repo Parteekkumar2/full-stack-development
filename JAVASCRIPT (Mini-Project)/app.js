47f578ecfd717157174e86ee8385bed8995fa9b9
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-05-30 09:42:25 +0530

    Add Button Event Listener concept on simon says game

--- JAVASCRIPT (Mini-Project)/app.js
+++ JAVASCRIPT (Mini-Project)/app.js
@@ -16,12 +16,20 @@ document.addEventListener("keypress",function(){
     }
 });
 
-function btnFlash(btn){
+function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
     },250);                  
 }
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },250);                  
+}
+
 
 function levelUp(){
     level++;
@@ -33,5 +41,15 @@ function levelUp(){
     console.log(randIdx);
     console.log(randColor);
     console.log(randBtn);
-    btnFlash(randBtn);
+    gameFlash(randBtn);
+}
+
+function btnPress(){
+    console.log(this);
+    let btn=this;
+    userFlash(btn);
+}
+let allBtns=document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
 }
