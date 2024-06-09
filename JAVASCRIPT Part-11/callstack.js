2e1da6056ef630f89ae49dc93e4c92dd0e71ea51
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-06-09 07:33:14 +0530

    Add callback Hell concept

--- JAVASCRIPT Part-11/callstack.js
+++ JAVASCRIPT Part-11/callstack.js
@@ -30,3 +30,23 @@ setTimeout(function(){
     console.log("apna college");
 },2000);
 console.log("Hello")
+
+//callbackHell 
+
+h1 = document.querySelector("h1");
+function changecolor(color,delay,nextcolorchange){
+    setTimeout(()=>{
+        h1.style.color=color;
+        if(nextcolorchange) nextcolorchange();
+    },delay);
+}
+
+changecolor("red",1000,()=>{
+   changecolor("orange",1000,()=>{  
+       changecolor("green",1000,()=>{
+           changecolor("yellow",1000,()=>{
+              changecolor("blue",1000);
+            });
+        });
+    });
+});
