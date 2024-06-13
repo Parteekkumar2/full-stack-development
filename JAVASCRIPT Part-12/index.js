063e43e28c9299fd6436ff047b89334e872b9aa4
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-06-13 08:14:22 +0530

    Add await keyword concept

--- JAVASCRIPT Part-12/index.js
+++ JAVASCRIPT Part-12/index.js
@@ -16,3 +16,52 @@ greet()
 let demo=async()=>{
     return 5;
 };
+
+
+
+//await keyword
+
+//eg-1
+
+function getnum(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let num=Math.floor(Math.random()*10)+1;
+            console.log(num);
+            resolve();
+        },1000);
+    });
+}
+
+async function demot(){
+   await getnum();
+   await getnum();
+    await getnum();
+    await getnum();
+}
+
+//eg-2
+h1=document.querySelector("h1");
+function changecolor(color,delay){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            h1.style.color = color;
+            console.log(`color changed to ${color}!`)
+            resolve("color change!");
+        },delay);
+    });
+}
+
+async function demos(){
+    await changecolor("red",1000);
+    await changecolor("orange",1000);
+    await changecolor("blue",1000);
+    await changecolor("yellow",1000);
+    await changecolor("pink",1000);
+    await changecolor("brown",1000);
+}
+
+
+
+
+
