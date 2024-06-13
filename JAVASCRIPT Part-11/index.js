00a1a0a5da230c7989843da41b9a80b1dd0a43ca
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-06-13 06:06:52 +0530

    Add old code

--- JAVASCRIPT Part-11/index.js
+++ JAVASCRIPT Part-11/index.js
@@ -28,3 +28,34 @@ savetoDb("parteek")
     console.log("promise was rejected");
     console.log("error of promise:",error);
 });
+
+
+//Refactoring old code 
+
+h1=document.querySelector("h1");
+function changecolor(color,delay){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            h1.style.color = color;
+            resolve("color change!");
+        },delay);
+    });
+}
+
+changecolor("red",1000)
+.then(()=>{
+console.log("red color was completed");
+return changecolor("orange",1000);
+})
+.then(()=>{
+    console.log("orange color was completed");
+    return changecolor("green",1000);
+    })
+    .then(()=>{
+        console.log("green color was completed");
+        return changecolor("blue",1000);
+        })
+
+        .then(()=>{
+            console.log("blue color was completed");
+            });
