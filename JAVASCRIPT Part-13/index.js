4958a9de285e92d2f3694c5cce820f2015e3f9e5
Parteek <159543478+Parteekkumar2@users.noreply.github.com>
2024-06-23 17:22:04 +0530

    Dog  picture api concept

--- JAVASCRIPT Part-13/index.js
+++ JAVASCRIPT Part-13/index.js
@@ -1,20 +1,44 @@
+// let btn=document.querySelector("button");
+// btn.addEventListener("click", async()=>{
+//     let fact=await getFacts();
+//     console.log(fact);
+//     let p=document.querySelector("#result");
+//     p.innerText=fact;
+// });
+
+// let urls="https://catfact.ninja/fact";
+// async function getFacts(){
+//    try{
+//     let res=await axios.get(urls);
+//     return res.data.fact;
+   
+//    }catch (e){
+//     console.log("error -",e);
+//     return "No fact found";
+//    }
+
+
+// }
+
+
+//Dog picture API
+
 let btn=document.querySelector("button");
+let urls2 ="https://dog.ceo/api/breeds/image/random";
 btn.addEventListener("click", async()=>{
-    let fact=await getFacts();
-    console.log(fact);
-    let p=document.querySelector("#result");
-    p.innerText=fact;
+    let link= await getImage();
+    let img=document.querySelector("#result");
+    img.setAttribute("src", link);
 });
 
-let urls="https://catfact.ninja/fact";
-async function getFacts(){
+async function getImage(){
    try{
-    let res=await axios.get(urls);
-    return res.data.fact;
+    let res=await axios.get(urls2);
+    return res.data.message;
    
    }catch (e){
     console.log("error -",e);
-    return "No fact found";
+    return "/";
    }
 
 
